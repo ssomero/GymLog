5594212656c71a0bb38923a0a8db860cad48cd74
ubuntu <sonja.somero@helsinki.fi>
2016-02-20 17:23:53 +0200

    Added some seed data - to be modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+e1 = Exercise.create name:"Ab roll", main_muscles:"Abdominal", bodypart:"Abs", force:"Pull", mechanics_type:"Compound"
+e2 = Exercise.create name:"Bench press", main_muscles:"Some chest muscle", bodypart:"Chest", force:"Push",
+                     mechanics_type:"Compound"
+e3 = Exercise.create name:"Deadlift", main_muscles:"Hamstrings, lower back", bodypart:"Lower body", force:"Pull",
+                     mechanics_type:"Compound"
+
+w1 = Workout.create name:"PPL" exercise_id:e1.id
+w2 = Workout.create name:"PHUL" exercise_id:e2.id
+w3 = Workout.create name:"PHAT" exercise_id:e3.id
