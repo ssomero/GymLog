4735c0628a0da220b4146ddcd116319b7ce04723
ubuntu <sonja.somero@helsinki.fi>
2016-02-28 14:28:03 +0200

    Added some views for routines

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -2,4 +2,8 @@ class RoutinesController < ApplicationController
   def index
 
   end
+
+  def new
+    @routine = Routine.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   resources :workouts
   resources :exercise_sets
   resources :exercises
+  resources :routines
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'exercises#index'
-
-  get 'routines' => 'routines#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
