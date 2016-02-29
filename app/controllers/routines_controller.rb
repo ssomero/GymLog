9afb922ea593aa90717a7e14af4f31de1c0a59a1
ubuntu <sonja.somero@helsinki.fi>
2016-02-29 21:04:16 +0200

    Refactoring

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,16 +1,17 @@
 class RoutinesController < ApplicationController
+  before_action :set_routine, only: [:show, :edit, :update, :destroy]
+  before_action :set_exercises, only: [:new, :edit]
+
   def index
     @routines = Routine.all
   end
 
   def show
-    @routine = Routine.find(params[:id])
   end
 
   def new
     @routine = Routine.new
     @routine.routine_sets.build
-    @exercises = Exercise.all
   end
 
   def create
@@ -32,6 +33,14 @@ class RoutinesController < ApplicationController
   def routine_params
     params.require(:routine).permit(:name, routine_sets_attributes: [:exercise_id, :routine_id, :sets, :repetition,
                                                                      :rest_time])
+  end
+
+  def set_routine
+  @routine = Routine.find(params[:id])
+  end
+
+  def set_exercises
+    @exercises = Exercise.all
   end
 
 end
